9a1d6367abb52be9676d67c00e3721b3d374dc1d
kaypee <resaba.kaypee@gmail.com>
2019-12-23 16:02:55 +0700

    setup auth context

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,26 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import IssueState from "./context/issue/IssueState";
+import AuthState from "./context/auth/AuthState";
 import "./App.css";
 
 function App() {
   return (
-    <IssueState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </IssueState>
+    <AuthState>
+      <IssueState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </IssueState>
+    </AuthState>
   );
 }
 
---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,11 @@
+import {
+  REGISTER_SUCCES,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from "../types";
+
