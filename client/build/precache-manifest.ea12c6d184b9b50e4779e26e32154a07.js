fcb6011af4e739630a8402844113d30b6f3e9b45
kaypee <resaba.kaypee@gmail.com>
2020-01-01 04:10:07 +0700

    created build folder for deploy

---
+++ client/build/precache-manifest.ea12c6d184b9b50e4779e26e32154a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404d2e93ec6601921d28e61f38f1212a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bf4b9cc84d4747a3f4a",
+    "url": "/static/css/main.2136176a.chunk.css"
+  },
+  {
+    "revision": "962747c90dd632ffe674",
+    "url": "/static/js/2.bbe36a8a.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.bbe36a8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bf4b9cc84d4747a3f4a",
+    "url": "/static/js/main.96979b79.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
